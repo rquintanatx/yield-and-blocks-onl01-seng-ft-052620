d2cf3371f89841819134468603a112bc0bcbb4fe
Robert Quintana <robertquintanatx@gmail.com>
2020-05-29 02:13:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t(haveto)
-  i = 0
-  while i < haveto.length 
-    yield haveto[i]
-    i = i + 1
+  if block_given?
+    i = 0 
+    while i < haveto.length 
+      yield haveto[i]
+      i = i + 1
+    end
+    haveto
+  else
+    puts "Hey! No block was given!"
   end
-  haveto
 end
 
 # call your method here!
